48864ea201d7da749be2334ea17a3556ba4bb518
Brad Miller <bonelake@mac.com>
2022-12-20 16:13:12 -0600

    Use background callback for first graph!

--- dashserver/app.py
+++ dashserver/app.py
@@ -4,11 +4,13 @@
 # Most of what we want to do will probably involve "long callbacks" https://dash.plotly.com/long-callbacks
 # this will avoid http timeouts
 
+import dash
 from dash import Dash, html, dcc
 from dash.long_callback import DiskcacheLongCallbackManager
 from dash.dependencies import Input, Output
 import diskcache
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -27,37 +29,56 @@ BASE = "py4e-int"
 colors = {"background": "#111111", "text": "#7FDBFF"}
 
 
-progress = pd.read_sql_query(
-    f"""select sub_chapter_id, status, count(*)
-from user_sub_chapter_progress where course_name = '{COURSE}' and chapter_id = '{CHAPTER}'
-group by sub_chapter_id, status
-order by sub_chapter_id""",
-    eng,
+@dash.callback(
+    output=Output("example-graph", "figure"),
+    inputs=Input("button_id", "n_clicks"),
+    background=True,
+    manager=long_callback_manager,
 )
-scdf = progress.groupby("sub_chapter_id").agg(total=("count", "sum"))
-pdf = progress.groupby(["sub_chapter_id", "status"]).agg(
-    students=("count", "sum"), ccount=("count", "min")
-)
-pdf = pdf.reset_index()
-pdf["pct"] = pdf.apply(lambda row: row.students / scdf.loc[row.sub_chapter_id], axis=1)
-smap = {-1: "Not Started", 0: "Started", 1: "Complete"}
-pdf["named_status"] = pdf.status.map(lambda x: smap[x])
+def do_callback(n_clicks):
+    return make_progress_graph(n_clicks)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
 
-fig = px.bar(
-    pdf, x="pct", y="sub_chapter_id", color="named_status", width=700, height=1000
-)
+def make_progress_graph(start):
+    if start:
+        fig = px.bar(x=["student"], y=[1])
+        return fig
 
-fig.update_layout(
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
-)
+    progress = pd.read_sql_query(
+        f"""select sub_chapter_id, status, count(*)
+    from user_sub_chapter_progress where course_name = '{COURSE}' and chapter_id = '{CHAPTER}'
+    group by sub_chapter_id, status
+    order by sub_chapter_id""",
+        eng,
+    )
+    scdf = progress.groupby("sub_chapter_id").agg(total=("count", "sum"))
+    pdf = progress.groupby(["sub_chapter_id", "status"]).agg(
+        students=("count", "sum"), ccount=("count", "min")
+    )
+    pdf = pdf.reset_index()
+    pdf["pct"] = pdf.apply(
+        lambda row: row.students / scdf.loc[row.sub_chapter_id], axis=1
+    )
+    smap = {-1: "Not Started", 0: "Started", 1: "Complete"}
+    pdf["named_status"] = pdf.status.map(lambda x: smap[x])
+
+    # assume you have a "long-form" data frame
+    # see https://plotly.com/python/px-arguments/ for more options
+
+    fig = px.bar(
+        pdf, x="pct", y="sub_chapter_id", color="named_status", width=700, height=1000
+    )
+
+    fig.update_layout(
+        plot_bgcolor=colors["background"],
+        paper_bgcolor=colors["background"],
+        font_color=colors["text"],
+    )
+    return fig
+
 
 app.layout = html.Div(
-    children=[
+    [
         html.H1(
             children="Student Progress",
             style={"textAlign": "center", "color": colors["text"]},
@@ -67,7 +88,8 @@ app.layout = html.Div(
         Dash: A web application framework for your data.
     """
         ),
-        dcc.Graph(id="example-graph", figure=fig),
+        dcc.Graph(id="example-graph"),
+        html.Button(id="button_id", children="update"),
     ]
 )
 
